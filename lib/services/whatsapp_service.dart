348976c3263b7d5c96e1fc09ecb54b8dfc69ea79
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-18 20:36:43 +0530

    Refactor WhatsAppService to improve message formatting and clean up code structure

--- lib/services/whatsapp_service.dart
+++ lib/services/whatsapp_service.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import '../models/loan_model.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../models/loan_model.dart';
 
 class WhatsAppService {
   static final currencyFormatter = NumberFormat.currency(
@@ -15,20 +16,19 @@ class WhatsAppService {
     try {
       // Format the message based on messageType
       String message = getMessageByType(loan, messageType);
-      
+
       // Format the phone number (remove spaces, +, etc)
       String phoneNumber = loan.whatsappNumber.replaceAll(RegExp(r'[^\d]'), '');
-      
+
       // If phone number doesn't start with country code, add Indian country code
       if (!phoneNumber.startsWith('91') && phoneNumber.length == 10) {
         phoneNumber = '91$phoneNumber';
       }
-      
+
       // Create WhatsApp URL
       final Uri whatsappUrl = Uri.parse(
-        'https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}'
-      );
-      
+          'https://wa.me/$phoneNumber?text=${Uri.encodeComponent(message)}');
+
       // Launch URL
       if (await canLaunchUrl(whatsappUrl)) {
         await launchUrl(
@@ -49,30 +49,31 @@ class WhatsAppService {
   // Get message by type
   static String getMessageByType(Loan loan, String messageType) {
     final formattedLoanAmount = currencyFormatter.format(loan.loanAmount);
-    final formattedRemainingAmount = currencyFormatter.format(loan.remainingAmount);
+    final formattedRemainingAmount =
+        currencyFormatter.format(loan.remainingAmount);
     final formattedDueDate = DateFormat('dd MMM yyyy').format(loan.dueDate);
-    
+
     switch (messageType) {
       case 'friendly':
         return 'Hi ${loan.borrowerName}, this is a friendly reminder about your loan of $formattedLoanAmount. '
             'Remaining balance: $formattedRemainingAmount. '
             'Please let me know when you can make the next payment. Thanks!';
-            
+
       case 'reminder':
         return 'Hello ${loan.borrowerName}, just a reminder that you have a pending loan balance of $formattedRemainingAmount. '
             'The loan is due on $formattedDueDate. '
             'Please arrange for payment at your earliest convenience. Thank you.';
-            
+
       case 'urgent':
         return 'Hello ${loan.borrowerName}, your loan payment of $formattedLoanAmount was due on $formattedDueDate. '
             'Current balance: $formattedRemainingAmount. '
             'Please contact me to discuss payment. Thank you.';
-            
+
       case 'confirmation':
         return 'Hi ${loan.borrowerName}, this is to confirm that I have received your recent payment. '
             'Your remaining balance is $formattedRemainingAmount. '
             'Thank you for your payment!';
-            
+
       default:
         return 'Hi ${loan.borrowerName}, regarding your loan of $formattedLoanAmount. '
             'Current balance: $formattedRemainingAmount. '
